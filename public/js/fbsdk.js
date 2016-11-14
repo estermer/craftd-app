0d67d4a6135068f73df1e75cf2f4eabf264d50f8
estermer <eric.stermer@gmail.com>
2016-11-14 14:15:12 -0500

    added boilerplate code for facebook sdk to fbsdk.js

---
+++ public/js/fbsdk.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1800503750205376',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+};
