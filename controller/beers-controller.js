475a27c72a3563f25edf03c9c2cad1c2be3bdb0e
estermer <eric.stermer@gmail.com>
2016-11-15 11:49:07 -0500

    Added a users and beers controller and exported router
    no routes were added yet to eaither, just boilerplate

---
+++ controller/beers-controller.js
@@ -0,0 +1,13 @@
+// do router requests and an api requests to brewery DB
+//ROUTER SETUP
+//=========================================
+var express = require('express');
+var router = express.Router();
+
+//EXTERNAL FILES
+//=========================================
+var Beer = require('../model/beer.js');
+
+
+
+module.exports = router;
---
+++ controller/users-controller.js
@@ -0,0 +1,14 @@
+//ROUTER SETUP
+//=========================================
+var express = require('express');
+var router = express.Router();
+
+//EXTERNAL FILES
+//=========================================
+var User = require('../model/user.js');
+var Beer = require('../model/beer.js');
+
+
+
+
+module.exports = router;
