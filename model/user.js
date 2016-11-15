3532bd04b738066dfa8b510bd4746e6aade8d5ca
estermer <eric.stermer@gmail.com>
2016-11-15 11:20:22 -0500

    create a user schema that contains a username, password and a array of beers from the beer schema
    exported the user model after created it based on user schema

---
+++ model/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+var Schema = mongoose.Schema;
+var BeerSchema = require('./beer.js').schema;
+
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+  beers: [BeerSchema]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+var UserModel = mongoose.model(UserSchema);
+
+module.exports = UserModel;
