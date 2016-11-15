5bac16e70d7b2d42cd006c15d53422f0ef27aa9a
estermer <eric.stermer@gmail.com>
2016-11-15 11:23:38 -0500

    Created a beer schema with a name, img, comment, rating then created the model based on the schema and exported both the model and the schema

--- model/beer.js
+++ model/beer.js
@@ -1,3 +1,18 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 var Schema = mongoose.Schema;
+
+var BeerSchema = new Schema({
+  name: String,
+  img: String,
+  comment: String,
+  rating: Integer
+});
+
+BeerSchema.plugin(passportLocalMongoose);
+BeerModel = mongoose.model('Beer', BeerSchema);
+
+module.exports = {
+  schema: BeerSchema,
+  model: BeerModel
+};
