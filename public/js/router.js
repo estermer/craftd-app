b86a359bc115e2aea34c8b78cb8931a81347293f
estermer <eric.stermer@gmail.com>
2016-11-16 11:49:33 -0500

    updated ui-router to get partials to work

--- public/js/router.js
+++ public/js/router.js
@@ -11,23 +11,23 @@
     $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: "/partials/home.html",
+      templateUrl: "partials/home.html",
     })
     .state('register', {
       url: '/register',
-      templateUrl: "/partials/register.html",
+      templateUrl: "partials/register.html",
     })
     .state('login', {
       url: '/login',
-      templateUrl: "/partials/login.html",
+      templateUrl: "partials/login.html",
     })
     .state('search', {
       url: '/search-beers',
-      templateUrl: "/partials/search-beers.html",
+      templateUrl: "partials/search-beers.html",
     })
     .state('user-home', {
       url: '/user-home',
-      templateUrl: "/partials/user-home.html",
+      templateUrl: "partials/user-home.html",
     });
 
     $urlRouterProvider.otherwise('/');
